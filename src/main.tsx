beb7188781d3c1ebc3122da8314a228ad3a55037
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-11-14 08:32:16 +0600

    material-ui set up

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
 
 import { store } from '@app/store.ts';
 import App from './App.tsx';
 
+import '@fontsource/roboto/cyrillic.css';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
+      <CssBaseline />
       <BrowserRouter>
         <App />
       </BrowserRouter>
