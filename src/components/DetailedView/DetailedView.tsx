3e87960a1f6528f41e59ed9d5193d8c12df00487
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-11-14 12:47:15 +0600

    DetailedView implemented

--- src/components/DetailedView/DetailedView.tsx
+++ src/components/DetailedView/DetailedView.tsx
@@ -1,10 +1,50 @@
-import { Box } from '@mui/material';
+import { useCallback, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import parse from 'html-react-parser';
+
+import { useAppDispatch, useAppSelector } from '@app/hooks';
+import { Selectors } from '@slices/details-slice';
+import { getTVShow } from '@thunks/details-thunks';
+
+import { Box, LinearProgress, Typography } from '@mui/material';
 
 const DetailedView = () => {
+  const tvShow = useAppSelector(Selectors.tvShow);
+  const loading = useAppSelector(Selectors.loading);
+
+  const { id } = useParams();
+
+  const dispatch = useAppDispatch();
+
+  const fetchData = useCallback(
+    async (id: string) => {
+      const _id = Number.parseInt(id);
+
+      await dispatch(getTVShow(_id));
+    },
+    [dispatch]
+  );
+
+  useEffect(() => {
+    if (id) {
+      fetchData(id);
+    }
+  }, [id, fetchData]);
+
   return (
-    <>
-      <Box sx={{ backgroundColor: 'black', height: '100vh' }} />
-    </>
+    <Box sx={{ py: 1 }}>
+      {loading ? (
+        <LinearProgress />
+      ) : tvShow ? (
+        <>
+          <img src={tvShow.image.medium} alt={tvShow.name} />
+          <Typography variant='h3'>{tvShow.name}</Typography>
+          {tvShow.summary && (
+            <Typography component='div'>{parse(tvShow.summary)}</Typography>
+          )}
+        </>
+      ) : null}
+    </Box>
   );
 };
 
