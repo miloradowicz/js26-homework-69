bc647e0217aeb1077383ac7860c74a89b6fb363d
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-11-14 11:08:33 +0600

    Header added

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <AppBar position='static'>
+      <Toolbar component='nav'>
+        <Typography
+          variant='h6'
+          component={Link}
+          color='white'
+          sx={{ textDecoration: 'none' }}
+          to='/'
+        >
+          TV Shows
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
